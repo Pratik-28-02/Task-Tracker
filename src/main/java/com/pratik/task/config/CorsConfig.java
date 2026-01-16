5ce35a6ca89e4b3b3fe6c16755eb4742e2bf11c7
Pratik-28-02 <pratikhegde1111@gmail.com>
2026-01-16 16:32:41 +0530

    Update CorsConfig.java

--- src/main/java/com/pratik/task/config/CorsConfig.java
+++ src/main/java/com/pratik/task/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/")
+                .allowedOrigins("http://localhost:5173",
+    "https://task-fe-9sf4-hml51vpgn-pratiks-projects-a2893bf9.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
